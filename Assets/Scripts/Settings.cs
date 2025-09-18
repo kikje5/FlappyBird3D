36800da5d21d75a2e1fdeceead487c4d6d91cfb6
jikke <jikke.egberts@hva.nl>
2025-09-18 14:13:55 +0200

    feat: settings change audio

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Settings : ScriptableObject
 {
-    
+    public AudioMixer audioMixer;
+
+    public int MasterVolume
+    {
+        get
+        {
+            audioMixer.GetFloat("Master", out var val);
+            return (int)(val + 10) * 5;
+        } 
+        set => audioMixer.SetFloat("Master", (value/5f) - 10);
+    }
+
+    public int MusicVolume
+    {
+        get
+        {
+            audioMixer.GetFloat("Music", out var val);
+            return (int)(val + 10) * 5;
+        }
+        set => audioMixer.SetFloat("Music", (value/5f) - 10);
+    }
+
+    public int SfxVolume
+    {
+        get
+        {
+            audioMixer.GetFloat("SFX", out var val);
+            return (int)(val + 10) * 5;
+        }
+        set => audioMixer.SetFloat("SFX", (value/5f) - 10);
+    }
 }
--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -9,30 +9,67 @@ public class MainMenu : MonoBehaviour
     private Button _settingsButton;
     private Button _exitButton;
     private Button _backToMenuButton;
-    private Slider _masterVolumeSlider;
-    private Slider _sfxVolumeSlider;
-    private Slider _musicVolumeSlider;
+    private SliderInt _masterVolumeSlider;
+    private SliderInt _sfxVolumeSlider;
+    private SliderInt _musicVolumeSlider;
     private VisualElement _mainMenu;
     private VisualElement _settingsMenu;
+    public Settings settings;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
         _uiDocument = GetComponent<UIDocument>();
-        _startButton = _uiDocument.rootVisualElement.Query<Button>("StartGameButton");
-        _settingsButton = _uiDocument.rootVisualElement.Query<Button>("SettingsButton");
-        _exitButton = _uiDocument.rootVisualElement.Query<Button>("ExitButton");
         _mainMenu  = _uiDocument.rootVisualElement.Query<VisualElement>("MainMenu");
         _settingsMenu = _uiDocument.rootVisualElement.Query<VisualElement>("SettingsMenu");
-        _settingsMenu.style.display = DisplayStyle.None;
-        _mainMenu.style.display = DisplayStyle.Flex;
         
-        _backToMenuButton =  _uiDocument.rootVisualElement.Query<Button>("BackToMain");
-        
+        _startButton = _mainMenu.Query<Button>("StartGameButton");
+        _settingsButton = _mainMenu.Query<Button>("SettingsButton");
+        _exitButton = _mainMenu.Query<Button>("ExitButton");
         _startButton.RegisterCallback<ClickEvent>(OnStartClick);
         _settingsButton.RegisterCallback<ClickEvent>(OnSettingsClick);
         _exitButton.RegisterCallback<ClickEvent>(OnExitClick);
+        
+        _backToMenuButton =  _settingsMenu.Query<Button>("BackToMain");
         _backToMenuButton.RegisterCallback<ClickEvent>(OnBackToMenuClick);
+        
+        print(_settingsMenu.childCount);
+        VisualElement sound = _settingsMenu.Query<VisualElement>("Sound");
+        print(sound.childCount);
+        
+        _masterVolumeSlider = sound.Query<SliderInt>("MasterSlider");
+        _musicVolumeSlider  = sound.Query<SliderInt>("MusicSlider");
+        _sfxVolumeSlider = sound.Query<SliderInt>("SFXSlider");
+        _masterVolumeSlider.RegisterCallback<ChangeEvent<int>>(OnMasterVolumeChange);
+        _musicVolumeSlider.RegisterCallback<ChangeEvent<int>>(OnMusicVolumeChange);
+        _sfxVolumeSlider.RegisterCallback<ChangeEvent<int>>(OnSfxVolumeChange);
+        
+        _settingsMenu.style.display = DisplayStyle.None;
+        _mainMenu.style.display = DisplayStyle.Flex;
+        
+        settings.MasterVolume = PlayerPrefs.GetInt("MasterVolume", 80);
+        settings.MusicVolume = PlayerPrefs.GetInt("MusicVolume", 80);
+        settings.SfxVolume = PlayerPrefs.GetInt("SfxVolume", 80);
+        
+        _masterVolumeSlider.value = settings.MasterVolume;
+        _musicVolumeSlider.value = settings.MusicVolume;
+        _sfxVolumeSlider.value = settings.SfxVolume;
+        
+    }
+
+    void OnMasterVolumeChange(ChangeEvent<int> changeEvent)
+    {
+        settings.MasterVolume = changeEvent.newValue;
+    }
+
+    void OnMusicVolumeChange(ChangeEvent<int> changeEvent)
+    {
+        settings.MusicVolume = changeEvent.newValue;
+    }
+
+    void OnSfxVolumeChange(ChangeEvent<int> changeEvent)
+    {
+        settings.SfxVolume = changeEvent.newValue;
     }
         
     void OnStartClick(ClickEvent evt)
@@ -48,6 +85,10 @@ public class MainMenu : MonoBehaviour
 
     void OnBackToMenuClick(ClickEvent evt)
     {
+        PlayerPrefs.SetInt("MasterVolume",  settings.MasterVolume);
+        print(settings.MasterVolume);
+        PlayerPrefs.SetInt("MusicVolume", settings.MusicVolume);
+        PlayerPrefs.SetInt("SfxVolume", settings.SfxVolume);
         _mainMenu.style.display = DisplayStyle.Flex;
         _settingsMenu.style.display = DisplayStyle.None;
     }
